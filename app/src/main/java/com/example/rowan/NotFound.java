76fcdbfa1f64b31612cb72716b7b0f55c2c88f5f
VanniMaceria <andreabucci123@gmail.com>
2023-02-18 17:40:40 +0100

    Aggiornamento del database e creazione barra di ricerca

---
+++ app/src/main/java/com/example/rowan/NotFound.java
@@ -0,0 +1,14 @@
+package com.example.rowan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class NotFound extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.not_found_layout);
+    }
+}
